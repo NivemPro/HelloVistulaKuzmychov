90ec04f1850518057d2d92064a41616499dce4e1
student <student@lab.pl.wsei>
2021-10-08 13:59:25 +0200

    Initial commit

---
+++ src/pl/vistula/helloworldkuzmychov/HelloWorldKuzmychov.java
@@ -0,0 +1,7 @@
+package pl.vistula.helloworldkuzmychov;
+
+public class HelloWorldKuzmychov {
+    public static void main(String[] args) {
+        System.out.println("Hello Vistula Kuzmychov");
+    }
+}
